eb60e3f112c72ffa04bae4575c954840b04d95a8
mephinet <mephinet@gmx.net>
2016-04-21 00:43:40 +0200

    first layer of indirection

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counters from './views/Counters';
 import counter from './counter';
+import {Map} from 'immutable';
 
-const store = createStore(counter);
+const app = (state = Map(), action)  => {
+    return state.set('counters', counter(state.get('counters'), action));
+};
+
+const store = createStore(app);
+
 const render = () => {
+    console.log(store.getState().toJS());
     ReactDOM.render(
-            <Counters value={store.getState()}
+            <Counters value={store.getState().get("counters")}
                addCounter={() => store.dispatch({type: 'ADD_COUNTER'})}
                onIncrement={i => store.dispatch({type: 'INCREMENT', id: i})}
                onDecrement={i => store.dispatch({type: 'DECREMENT', id: i})}
